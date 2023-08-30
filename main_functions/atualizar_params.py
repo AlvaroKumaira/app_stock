48e9af1507095bf24e7e86504374a1e9a442b697
Álvaro Kumaira <alvaro.kumaira@gmail.com>
2023-08-30 14:41:46 -0300

    Changes to the merge_sheets function

--- main_functions/atualizar_params.py
+++ main_functions/atualizar_params.py
@@ -24,6 +24,10 @@ def merge_sheets(excel_path):
     df3 = pd.read_excel(excel_path, sheet_name="P_N")
     df1 = pd.read_excel(excel_path, sheet_name="P_ESP")
 
+    df1 = df1.drop(columns='Descrição')
+    df2 = df2.drop(columns='Descrição')
+    df3 = df3.drop(columns='Descrição')
+
     # Rename columns for clarity
     df1.rename(columns={"cod_agrup": "B1_ZGRUPO"}, inplace=True)
     df2.rename(columns={"cod_agrup": "B1_ZGRUPO"}, inplace=True)
@@ -38,12 +42,12 @@ def merge_sheets(excel_path):
     # Add a temporary column to df3 to help identify its rows after merging
     df3['TempN_Comprar'] = 1
 
-    # Merge all three dataframes on cod_agrup using outer join
-    merged_df = df1.merge(df2, on=['B1_ZGRUPO', 'Descrição'], how='left')
-    merged_df = merged_df.merge(df3, on=['B1_ZGRUPO', 'Descrição'], how='left')
+    # Merge all three dataframes on B1_ZGRUPO using outer join
+    merged_df = df1.merge(df2, on='B1_ZGRUPO', how='outer')
+    merged_df = merged_df.merge(df3, on='B1_ZGRUPO', how='outer')
     merged_df.dropna(subset=['B1_ZGRUPO'], inplace=True)
 
-    # If TempN_Comprar is NaN, it means cod_agrup is not from df3
+    # If TempN_Comprar is NaN, it means B1_ZGRUPO is not from df3
     merged_df['n_comprar'] = merged_df['TempN_Comprar'].fillna(0).astype(int)
     merged_df['mult'] = merged_df['mult'].fillna(1).astype(int)
     
@@ -51,7 +55,12 @@ def merge_sheets(excel_path):
     merged_df.drop('TempN_Comprar', axis=1, inplace=True)
 
     # Fill NaN values with appropriate defaults
-    merged_df.fillna({"Descrição": "", "mult": 1, "seguranca": 0, "n_comprar": 0}, inplace=True)
-    print(merged_df[merged_df['B1_ZGRUPO'] == '81852589'])
+    merged_df.fillna({ "mult": 1, "seguranca": 0, "n_comprar": 0}, inplace=True)
+
+    # Group by 'B1_ZGRUPO' and 'Descrição' and get indices of rows with the highest 'seguranca' value
+    idx = merged_df.groupby('B1_ZGRUPO')['seguranca'].idxmax()
+
+    # Filter dataframe using these indices
+    merged_df = merged_df.loc[idx]
 
     return merged_df
